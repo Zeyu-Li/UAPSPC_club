a3969dc5b7308933e67e429d9af0a3a1e11fc04c
Andrew Li <34993025+Zeyu-Li@users.noreply.github.com>
2019-09-28 23:41:49 -0600

    vs code stuff

--- cpp_boilerplate/src/templete.cpp
+++ cpp_boilerplate/src/templete.cpp
@@ -1,9 +1,17 @@
-// This program *
 #include <cstdio>
+#include <cmath>
+
 using namespace std;
 
-int main()
-{
-    
+int a,b,c;
+
+int main(){
+    scanf("%d%d%d",&a,&b,&c);
+    long double v=(long double)sqrt((long double)c/(long double)a);
+
+    long double f=(long double)a*v*v+(long double)b*v+(long double)c;
+
+    printf("%.10lf\n",(double)v);
+    printf("%.10lf\n",(double)(v/f));
     return 0;
 }
